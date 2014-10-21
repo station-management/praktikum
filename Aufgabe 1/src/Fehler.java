d4cdf28afdb6e2ca58acbb975a5f114dcfcf1a61
station-management <s.ewerts@t-online.de>
2014-10-21 13:11:22 +0200

    Sarah Ewerts - Hausaufgabe 1

---
+++ Aufgabe 1/src/Fehler.java
@@ -0,0 +1,21 @@
+
+class Fehler {
+
+	Integer variable1;
+	
+	Integer variable2;
+	
+	Integer variable3;
+	
+	void methode1() {
+		System.out.println("Methode 1 aufgerufen");
+	}
+	
+	void methode2() {
+		System.out.println("Methode 2 aufgerufen");
+	}
+	void methode3() {
+		System.out.println("Methode 3 aufgerufen");
+	}
+	
+}
